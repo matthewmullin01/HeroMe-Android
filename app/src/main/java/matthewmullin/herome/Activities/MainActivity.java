f9af24b07063ba2d26ab4597a9fc4f4ff3da99bb
Matthew Mullin <Matthew Mullin>
2017-03-22 23:16:54 +0200

    Landing page GUI almost done

---
+++ app/src/main/java/matthewmullin/herome/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package matthewmullin.herome.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import matthewmullin.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
